20f82c8fdb2ba3e7b2023b7261bc4413e7687774
Jonas Yde <yde.jonas@outlook.com>
2017-04-11 10:58:50 +0200

    Basis aanpassingen Startup.cs

--- ContactManager/Startup.cs
+++ ContactManager/Startup.cs
@@ -7,11 +7,23 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace ContactManager
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+
+        }
+
+        public IConfigurationRoot Configuration { get; private set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -19,13 +31,24 @@ namespace ContactManager
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(
+                    new ExceptionHandlerOptions()
+                    {
+                        ExceptionHandler = async (context) => await context.Response.WriteAsync("Er is iets fout gegaan. Contacteer de site administrator.")
+                    });
             }
 
             app.Run(async (context) =>
