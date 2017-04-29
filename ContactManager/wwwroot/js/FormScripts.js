0e6ff9361999d88dbedb8fa6b8a58df13dd4f268
Jonas Yde <yde.jonas@outlook.com>
2017-04-29 18:34:59 +0200

    Fix layout

---
+++ ContactManager/wwwroot/js/FormScripts.js
@@ -0,0 +1,46 @@
+﻿"use strict";
+
+var telefoonButtonCounter = 0;
+
+
+function nieuweTelefoon() {
+    const nieuweTelefoonNaamVeldString = "<input type=\"text\" name=\"telefoonsNamen\" />";
+    const nieuweTelefoonNummerVeldString = "<input type=\"text\" name=\"telefoonNummers\" />";
+
+    const nieuweTelefoonVelden = document.createElement("p");
+    nieuweTelefoonVelden.innerHTML = "Naam: " + nieuweTelefoonNaamVeldString + "Nummer:  " + nieuweTelefoonNummerVeldString;
+    const telefoonVeldenDiv = document.getElementById("telefoonVelden");
+
+    insertAfter(nieuweTelefoonVelden, telefoonVeldenDiv.lastChild);
+
+    telefoonButtonCounter++;
+
+
+}
+
+const toonContactPersonen = function () {
+    const zoekString = $('#zoekveld').val();
+    var contactLijstDiv = $('<div id="contactLijst"></div>');
+
+    $.get("/Nieuw/Personen?naam=" + zoekString, function (data, status) {
+        const personen = data;
+        console.log(personen.length + "resultaten gevonden");
+
+        for (let i = 0; i < personen.length; i++) {
+            const tableRow = $('<p><input type="radio" name="contactPersoonId" value="' + personen[i].id + '"> ' + personen[i].naam + '</p>');
+            contactLijstDiv.append(tableRow);
+        }
+    });
+    $("#contactLijst").replaceWith(contactLijstDiv);
+};
+
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+$(document).ready(function () {
+    $(".datumKiezer").datepicker();
+
+    $("#zoekveld").keydown(toonContactPersonen);
+
+});
